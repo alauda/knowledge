2f764f5229bcb5ec8f7c304e541fe1213a10c2b9
yangxiaolang <1810849666@qq.com>
2025-08-29 17:42:20 +0800

    style: remove dark style for only light mode

--- theme/components/Search/index.tsx
+++ theme/components/Search/index.tsx
@@ -17,7 +17,7 @@ const Search: React.FC<SearchProps> = ({
     <div className={`relative max-w-full ${className}`}>
       <div className="flex items-center bg-[var(--rp-c-bg-mute)] rounded-[var(--rp-radius)] border border-gray-300  px-4 py-2 shadow-sm transition-all duration-200 focus-within:border-blue-500 focus-within:ring-1 focus-within:ring-blue-500 hover:shadow-md">
         <svg
-          className="h-5 w-5 flex-shrink-0 text-gray-400 dark:text-gray-400 mr-2"
+          className="h-5 w-5 flex-shrink-0 text-gray-400 mr-2"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
@@ -35,7 +35,7 @@ const Search: React.FC<SearchProps> = ({
           value={value}
           onChange={(e) => onSearch(e.target.value)}
           placeholder={placeholder}
-          className="ml-3 w-full border-0 bg-transparent p-0 text-gray-900 placeholder-gray-400 outline-none focus:ring-0 dark:text-gray-100 dark:placeholder-gray-500"
+          className="ml-3 w-full border-0 bg-transparent p-0 text-gray-900 placeholder-gray-400 outline-none focus:ring-0 "
         />
       </div>
     </div>
